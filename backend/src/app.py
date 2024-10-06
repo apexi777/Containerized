b0efd8f04e39981b808373d60c5e33dcca30eb57
apexi777 <apexi7771@gmail.com>
2024-10-06 18:12:16 +0300

    add env.example file and change app.py

--- backend/src/app.py
+++ backend/src/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from flask_mysqldb import MySQL
 from flask_cors import CORS, cross_origin
@@ -6,10 +7,10 @@ app = Flask(__name__)
 CORS(app)
 
 # Конфигурация базы данных MySQL
-app.config['MYSQL_HOST'] = 'database'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'password'
-app.config['MYSQL_DB'] = 'users'
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST', 'localhost')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER', 'root')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD', 'password')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB', 'users')
 
 mysql = MySQL(app)
 
